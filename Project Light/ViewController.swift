825fce769ba75ba998d6b0cbbd70a3f00aa798d2
Paul Franco <mail@paulfran.co>
2020-07-21 15:06:45 -0700

    Initial Commit

---
+++ Project Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Light
+//
+//  Created by Paul Franco on 7/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
