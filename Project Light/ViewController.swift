720dec08c625d0d685017577fc1dc512758028dc
Paul Franco <mail@paulfran.co>
2020-07-21 15:55:02 -0700

    completed app

--- Project Light/ViewController.swift
+++ Project Light/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lightButton: UIButton!
+    
+    var lightOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+            
+        }
+        
+    }
+    
 }
 
